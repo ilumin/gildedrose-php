7339ec332bbbf09d2d40c350262a78122072ebe3
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-04-04 16:39:21 +0700

    UPDATE fixture

--- src/texttest_fixture.php
+++ src/texttest_fixture.php
@@ -11,15 +11,15 @@ $items = array(
     new Item('Sulfuras, Hand of Ragnaros', 0, 80),
     new Item('Sulfuras, Hand of Ragnaros', -1, 80),
     new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
+    new Item('Backstage passes to a TAFKAL80ETC concert', 10, 20),
+    new Item('Backstage passes to a TAFKAL80ETC concert', 5, 10),
     // this conjured item does not work properly yet
     new Item('Conjured Mana Cake', 3, 6)
 );
 
 $app = new GildedRose($items);
 
-$days = 2;
+$days = 6;
 if (count($argv) > 1) {
     $days = (int) $argv[1];
 }
