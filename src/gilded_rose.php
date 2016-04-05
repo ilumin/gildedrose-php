03ed3ad4feb40b81b72b8f6f70a918932f77f9e0
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-04-05 14:37:38 +0700

    REMOVE unused method

--- src/gilded_rose.php
+++ src/gilded_rose.php
@@ -101,64 +101,6 @@ class GildedRose {
                 break;
         }
     }
-
-    /**
-     * Update item quality before sell_in update
-     *
-     * @param Item $item
-     */
-    public function updateQualityBeforeSellIn(Item $item)
-    {
-        if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if ($item->quality > 0) {
-                if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                    $item->quality = $item->quality - 1;
-                }
-            }
-        } else {
-            if ($item->quality < 50) {
-                $item->quality = $item->quality + 1;
-                if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($item->sell_in < 11) {
-                        if ($item->quality < 50) {
-                            $item->quality = $item->quality + 1;
-                        }
-                    }
-                    if ($item->sell_in < 6) {
-                        if ($item->quality < 50) {
-                            $item->quality = $item->quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    /**
-     * Update item quality after update sell_in
-     *
-     * @param Item $item
-     */
-    public function updateQualityAfterSellIn($item)
-    {
-        if ($item->sell_in < 0) {
-            if ($item->name != 'Aged Brie') {
-                if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($item->quality > 0) {
-                        if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                            $item->quality = $item->quality - 1;
-                        }
-                    }
-                } else {
-                    $item->quality = $item->quality - $item->quality;
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                }
-            }
-        }
-    }
 }
 
 class Item {
